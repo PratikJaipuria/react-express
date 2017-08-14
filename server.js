9ac767572c76be71fccf94fda8dfdc9eb34a1127
jaipuriapratik <jaipuria.p@husky.neu.edu>
2017-08-14 13:31:58 -0400

    updated the mongo string to local DB

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ var router = express.Router();
 // Setup logger
 // app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
-var mongoDB = 'mongodb://<dbuser>:<dbpassword>@ds151108.mlab.com:51108/heroku_svls88p3';
+// var mongoDB = 'mongodb://<dbuser>:<dbpassword>@ds151108.mlab.com:51108/heroku_svls88p3';
+var mongoDB = 'mongodb://127.0.0.1:27017/reactExpres';
 mongoose.connect(mongoDB, { useMongoClient: true })
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
